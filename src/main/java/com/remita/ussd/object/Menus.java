1a41ea7fbbf9b15baf35b12991b4dabd016152fa
ALOZIE michael <ALOZIE michael>
2017-11-08 19:51:20 +0100

    added main menus

--- src/main/java/com/remita/ussd/object/Menus.java
+++ src/main/java/com/remita/ussd/object/Menus.java
@@ -103,6 +103,3 @@ public class Menus {
         return request;
     }
 }
-
-
-newRequest.setUssdContent(" Welcome to Remita \n\n 1> Transfer \n 2> Airtime \n 3> Balance \n 4> Pay TSA and Billers \n 5> Get loan \n 6> RRR \n 7> Receipt \n 8> Register \n\n 9> Next ");
