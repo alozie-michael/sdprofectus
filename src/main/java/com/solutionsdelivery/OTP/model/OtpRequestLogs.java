fcce0ef907d293e484c9207f84578817cb7a2892
ALOZIE michael <ALOZIE michael>
2018-02-16 12:10:39 +0100

    updated otprequestlogs entity columns

--- src/main/java/com/solutionsdelivery/OTP/model/OtpRequestLogs.java
+++ src/main/java/com/solutionsdelivery/OTP/model/OtpRequestLogs.java
@@ -14,10 +14,17 @@ class OtpRequestLogs {
     private Long id;
 
     private String bank;
+
+    @Column(name = "ACCOUNTNUMBER")
     private String accountNumber;
+
     private String request;
+    @Column(name = "REQUESTTIMESTAMP")
     private String requestTimeStamp;
+
     @Column(length = 500)
     private String response;
+
+    @Column(name = "RESPONSETIMESTAMP")
     private String responseTimeStamp;
 }
